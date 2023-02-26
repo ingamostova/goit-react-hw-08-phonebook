ba4f8424adfb359605be70e18447d8d44c1429ff
Інга Мостова <ingo4ka20041997@gmail.com>
2023-02-26 15:31:17 +0200

    fixes

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -22,14 +22,23 @@ export const contactsSlice = createSlice({
       state.isLoading = false;
       state.error = action.payload;
     },
+    [addContact.pending](state, action) {
+      state.isLoading = true;
+    },
     [addContact.fulfilled](state, action) {
+      state.isLoading = false;
       state.error = null;
       state.items.push(action.payload);
     },
     [addContact.rejected](state, action) {
+      state.isLoading = false;
       state.error = action.payload;
     },
+    [deleteContact.pending](state, action) {
+      state.isLoading = true;
+    },
     [deleteContact.fulfilled](state, action) {
+      state.isLoading = false;
       state.error = null;
       // const index = state.items.findIndex(
       //   contact => contact.id === action.payload.id
@@ -40,6 +49,7 @@ export const contactsSlice = createSlice({
       );
     },
     [deleteContact.rejected](state, action) {
+      state.isLoading = false;
       state.error = action.payload;
     },
   },
